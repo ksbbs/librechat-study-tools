ab851d099e2aacc3e51c5942c967ca61932700e4
ksbbs <99734497+ksbbs@users.noreply.github.com>
2026-03-22 09:13:08 +0800

    remove: old files

--- src/tools/index.ts
+++
@@ -1,4 +0,0 @@
-export { geogebraPlotTool } from './geogebra-plot.js';
-export { geogebra3DTool } from './geogebra-3d.js';
-export { physicsSimTool } from './physics-sim.js';
-export { mermaidDiagramTool } from './mermaid-diagram.js';
